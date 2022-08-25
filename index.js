518016483516b0ac8683a375bb9477716b2e6b62
frank <frank@ddxasia.com>
2022-08-25 20:32:22 +0700

    Adding updates to branch

--- index.js
+++ index.js
@@ -1,52 +1,130 @@
-const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
-    "/"];
-//password array//
-let password = [];
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
 
-//GET 32 THINGS AND METHOD THEM INTO 2 NEW ARRAYS!!
-//LET password = ARRAY
-//LET PASSWORDTWO = ARRAY 
-// let passwordTwo = [];
+let pwordOne = document.getElementById("pwordone");
+let pwordTwo = document.getElementById("pwordtwo");
 
-let pwordOne = document.getElementById("pwordone")
-let pwordTwo = document.getElementById("pwordtwo")
+const generateRandomArray = () => {
+  const randomArray = [];
+  const getRandomLetter = (array) => {
+    let randomIndex = Math.floor(Math.random() * characters.length);
+    let randomLetter = characters[randomIndex];
+    randomArray.push(randomLetter);
+    return randomArray;
+  };
 
-function getRandomLetter() {
-    let randomIndex = Math.floor(Math.random() * characters.length)
-    let randomLetter = characters[randomIndex]
-    password.push(randomLetter)
-    // passwordTwo.push(randomLetter)
-    // return randomLetter
-    return randomLetter
-}
+  for (let i = 0; i < 30; i++) {
+    getRandomLetter(i);
+  }
+  return randomArray;
+};
 
-// //loop random letters x times//
+const startGen = () => {
+  const password = generateRandomArray();
+  let passwordOneArray = password.slice(Math.random(), 16);
+  let passwordOne = passwordOneArray.join("");
+  let passwordTwoArray = password.slice(16, 30);
+  let passwordTwo = passwordTwoArray.join("");
 
-for (let i = 0; i < 30; i++) {
-    getRandomLetter(i)
-    // return randomLetter
-}
+  pwordOne.textContent = passwordOne;
+  pwordTwo.textContent = passwordTwo;
 
-
-
-
-//make password, get long array and take off commas, then return
-//array through get passwords function and show it on screen//
-function startGen() {
-
-    let passwordOneArray = password.slice(0, 16)
-    let passwordOne = passwordOneArray.join('')
-    let passwordTwoArray = password.slice(16, 30)
-    let passwordTwo = passwordTwoArray.join('')
-
-    pwordOne.textContent = passwordOne
-    pwordTwo.textContent = passwordTwo
-
-    console.log(password)
-
-}
+  console.log(passwordOne, "passwordOne");
+  console.log(passwordTwo, "passwordTwo");
+};
 
 function getPasswords() {
-    return password
-    // return passwordTwo
+  return password;
+  // return passwordTwo
 }
